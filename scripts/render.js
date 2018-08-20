b91f91940be8cb2c1d8a031900ff07fbd1c2f30b
Fredrik Borgen Tørnvall <toernvall@gmail.com>
2018-08-20 16:04:16 +0200

    added open and close of new dialog in render.js

--- scripts/render.js
+++ scripts/render.js
@@ -17,20 +17,38 @@ function render() {
     if(currentstate === states.FlappyNerd){
         s_text.FlappyNerd.draw(ctx, width2 - s_text.FlappyNerd.width / 2, height - 360);
         s_buttons.Start.draw(ctx, startbtn.x, startbtn.y);
+
+        highScore = document.getElementById('highScore');
+        highScore.close();
     }
 
     if (currentstate === states.Splash) {
         s_splash.draw(ctx, width2 - s_splash.width / 2, height - 300);
         s_text.GetReady.draw(ctx, width2 - s_text.GetReady.width / 2, height - 380);
+        highScore = document.getElementById('highScore');
+        highScore.close();
     }
 
     if (currentstate === states.Score) {
-        s_text.GameOver.draw(ctx, width2 - s_text.GameOver.width / 2, height - 400);
-        s_score.draw(ctx, width2 - s_score.width / 2, height - 340);
+        s_text.GameOver.draw(ctx, width2 - s_text.GameOver.width / 2, height - 600);
+        s_score.draw(ctx, width2 - s_score.width / 2, height - 540);
         s_buttons.Ok.draw(ctx, okbtn.x, okbtn.y);
 
-        s_numberS.draw(ctx, width2 - 47, height - 304, score, null, 10);
-        s_numberS.draw(ctx, width2 - 47, height - 262, best, null, 10);
+        
+        s_numberS.draw(ctx, width2 - 47, height - 504, score, null, 10);
+        s_numberS.draw(ctx, width2 - 47, height - 462, best, null, 10);
+
+        var x = document.getElementById("highScore").open;
+        if(x === false){
+            highScore.showModal();
+            document.getElementById("playerName").value = " ";
+            document.getElementById("playerEmail").value = " ";
+            document.getElementById("score").innerHTML = score;
+        }
+        
+        //highScore = document.getElementById('highScore');
+        
+
     } else {
         s_numberB.draw(ctx, null, 20, score, width2);
     }
