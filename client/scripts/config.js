b283b2d72c601625789a679286ace63d5d34bd1a
Sindre Nyborg Berge <snb@itverket.no>
2018-08-29 17:40:42 +0200

    Add getting of highscore-list from backend

--- client/scripts/config.js
+++ client/scripts/config.js
@@ -1,6 +1,14 @@
+// //This file is ignroed in gitignore, to avoid checking in apiKey etc.
+// const config = {
+//   // baseUrl: "https://flappy-nerd.azurewebsites.net/api/",
+//   baseUrl: "http://localhost:7071/api/",
+//   apiKey: "<replace with function key>"
+// };
+
+
 //This file is ignroed in gitignore, to avoid checking in apiKey etc.
 const config = {
-  // baseUrl: "https://flappy-nerd.azurewebsites.net/api/",
-  baseUrl: "http://localhost:7071/api/",
-  apiKey: "<replace with function key>"
-};
+  baseUrl: "https://flappy-nerd.azurewebsites.net/api/",
+  // baseUrl: "http://localhost:7071/api/",
+  apiKey: "6WQ5BsRP08zWwCWQRId7bWJEvSRsBtX4V31w1JdofnacSy071b4G/A=="
+ };
