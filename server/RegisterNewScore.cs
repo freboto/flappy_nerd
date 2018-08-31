eb9b15cb83056e31a68d66ce689a56d3989f1071
Anders Kofoed <anders.kfd@gmail.com>
2018-08-31 19:56:29 +0200

    Add validation an trucat of username

--- server/RegisterNewScore.cs
+++ server/RegisterNewScore.cs
@@ -1,9 +1,11 @@
 
 using System;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -22,20 +24,19 @@ namespace FlappyNerd
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]HttpRequestMessage req, ILogger log,
             [Table("UserScores")]CloudTable scores)
         {
-            log.LogInformation("Register new score request recieved");
-
             string jsonContent = await req.Content.ReadAsStringAsync();
             dynamic data = JsonConvert.DeserializeObject(jsonContent);
-            var username = data.username.ToString().Trim().ToLower();
-            var email = data.email.ToString().Trim().ToLower();
-            var score = data.score;
+            string username = data.username.ToString();
+            string email = data.email.ToString().Trim().ToLower();
+            int score = data.score;
 
             if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(email)) return new BadRequestResult();
 
             await scores.ExecuteAsync(TableOperation.Insert(new UserScore
             {
                 RowKey = Guid.NewGuid().ToString(),
-                PartitionKey = username,
+                PartitionKey = "nerd",
+                Username = DemystifyUsername(username),
                 Email = email,
                 Score = score
             }));
@@ -44,6 +45,14 @@ namespace FlappyNerd
             {
                 Content = new StringContent("New score registered")
             };
+        }
+
+        public static string DemystifyUsername(string username)
+        {
+            Regex reg = new Regex("[^a-zA-Z']");
+            var trimmedLowerCase = username.Trim().ToLower();
+            trimmedLowerCase = reg.Replace(trimmedLowerCase, string.Empty);
+            return new string(trimmedLowerCase.Take(10).ToArray());
         }
     }
 }
--- server/UserScore.cs
+++ server/UserScore.cs
@@ -4,7 +4,18 @@ namespace FlappyNerd
 {
     public class UserScore : TableEntity
     {
-        public string Username => PartitionKey;
+        private string username;
+        public string Username
+        {
+            get
+            {
+                return this.PartitionKey != "nerd" ? this.PartitionKey : username;
+            }
+            set
+            {
+                this.username = value;
+            }
+        }
         public string Email { get; set; }
         public int Score { get; set; }
     }
